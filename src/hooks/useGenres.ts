a4e0d3fcba0d5471bd96e0a12dbf3c9f571cec7c
Miguel Imbach-Garuda <miguel.imbach@gmail.com>
2023-07-03 09:44:55 -0600

    Refactor Reusable ApiClient

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,15 +1,15 @@
 import { useQuery } from '@tanstack/react-query';
 import genres from '../data/genres';
-import apiClient, { FetchResponse } from '../services/api-client';
+import APIClient from '../services/api-client';
 import { Genre } from '../services/genreService';
 
-//const useGenres = () => ({data:genres, isLoading: false, error: null})
+
+
+const apiClient = new APIClient<Genre>('/genres');
 
 const useGenres = () => useQuery({
         queryKey: ['genres'],
-        queryFn: () =>
-                apiClient
-                        .get<FetchResponse<Genre>>('/genres').then(res => res.data),
+        queryFn: apiClient.getAll,
         staleTime: 1000 * 60 * 60 * 24, // this si set to 24 hours
         initialData: { count: genres.length, results: genres },
 })
