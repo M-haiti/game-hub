11c2279ec178f326ee3ebfce4e88d17911ca7213
Miguel Imbach-Garuda <miguel.imbach@gmail.com>
2023-07-04 09:46:29 -0600

    Implement infinite Scroll

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,10 +1,11 @@
-import { SimpleGrid, Text, Button, Box } from "@chakra-ui/react";
+import { SimpleGrid, Spinner, Text } from "@chakra-ui/react";
+import React from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
 import { GameQuery } from "../App";
 import useGames from "../hooks/useGames";
 import GameCard from "./GameCard";
 import GameCardContainer from "./GameCardContainer";
 import GameCardSkeleton from "./GameCardSkeleton";
-import React from "react";
 interface Props {
   gameQuery: GameQuery;
 }
@@ -13,10 +14,18 @@ const GameGrid = ({ gameQuery }: Props) => {
   const { data, error, isLoading, isFetchingNextPage, fetchNextPage, hasNextPage } = useGames(gameQuery);
   const skeletons = [1, 2, 3, 4, 5, 6];
 
+  const fetchedGamesCount = data?.pages.reduce((total, page) => total + page.results.length, 0) || 0;
+
   if (error) return <Text>{error.message}</Text>;
   return (
-    <Box>
-      <SimpleGrid columns={{ sm: 1, md: 2, xl: 4 }} spacing={6}>
+    <InfiniteScroll
+      dataLength={fetchedGamesCount}
+      hasMore={!!hasNextPage}
+      next={() => fetchNextPage()}
+      loader={<Spinner />}
+
+    >
+      <SimpleGrid columns={{ sm: 1, md: 2, xl: 4 }} spacing={6} padding="10px">
         {isLoading &&
           skeletons.map((skeleton) => (
             <GameCardContainer key={skeleton}>
@@ -31,8 +40,7 @@ const GameGrid = ({ gameQuery }: Props) => {
           </React.Fragment>
         )}
       </SimpleGrid>
-      {hasNextPage && <Button onClick={() => fetchNextPage()} marginY={5}>{isFetchingNextPage ? 'Loading...' : "Load More"}</Button>}
-    </Box>
+    </InfiniteScroll >
   );
 };
 
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -32,5 +32,6 @@ const useGames = (gameQuery: GameQuery) =>
       //If next is null meand its last page
       return lastPage.next ? allPages.length + 1 : undefined;
     },
+    staleTime: 24 * 60 * 60 * 1000,
   })
 export default useGames
