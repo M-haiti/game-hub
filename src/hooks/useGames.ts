37557d7475399952d623991c0df259f4b83a3998
Miguel Imbach-Garuda <miguel.imbach@gmail.com>
2023-07-03 19:12:03 -0600

    Impement Infinite Queries

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,4 +1,4 @@
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useInfiniteQuery } from "@tanstack/react-query";
 import { GameQuery } from "../App";
 import APIClient, { FetchResponse } from "../services/api-client";
 import { Platform } from "../hooks/usePlatforms";
@@ -16,16 +16,21 @@ export interface Game {
 }
 
 const useGames = (gameQuery: GameQuery) =>
-  useQuery<FetchResponse<Game>, Error>({
+  useInfiniteQuery<FetchResponse<Game>, Error>({
     queryKey: ["games", gameQuery],
-    queryFn: () =>
+    queryFn: ({ pageParam = 1 }) =>
       apiClient.getAll({
         params: {
           genres: gameQuery.genre?.id,
           platforms: gameQuery.platform?.id,
           ordering: gameQuery.sortOrder,
-          search: gameQuery.searchText
+          search: gameQuery.searchText,
+          page: pageParam,
         },
-      })
+      }),
+    getNextPageParam: (lastPage, allPages) => {
+      //If next is null meand its last page
+      return lastPage.next ? allPages.length + 1 : undefined;
+    },
   })
 export default useGames
